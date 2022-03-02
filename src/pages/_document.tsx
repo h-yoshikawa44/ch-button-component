ff716b82906d52d6fe7f9f685763875939923233
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2022-03-02 10:30:26 +0900

    fix: クローラー除外設定追加、langをjaに修正、フォント読み込み箇所を修正(#16)

---
+++ src/pages/_document.tsx
@@ -0,0 +1,30 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="ja">
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin=""
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&family=Noto+Sans+JP:wght@300;400;500&family=Poppins:wght@300;400;500;600&family=Ubuntu+Mono&display=swap"
+            rel="stylesheet"
+          />
+          <link rel="icon" href="/favicon.ico" />
+          <meta name="robots" content="noindex, nofollow" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
